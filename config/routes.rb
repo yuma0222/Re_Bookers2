1ecbba45c68526eff2675109329be60b3f3c90f3
yuma0222 <nakamura_poison@icloud.com>
2020-07-08 21:19:13 +0900

    [Finish]task3

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/about' => 'home#about'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
