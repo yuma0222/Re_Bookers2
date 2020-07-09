0583c0807a46f3040f2cbdf4cd333fadb32077e5
yuma0222 <nakamura_poison@icloud.com>
2020-07-10 01:50:57 +0900

    [Finish]re_task4

--- db/migrate/20200624071849_add_user_info_to_users.rb
+++ db/migrate/20200624071849_add_user_info_to_users.rb
@@ -1,6 +1,5 @@
 class AddUserInfoToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :introduction, :string
-    add_column :users, :profile_image_id, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_071849) do
+ActiveRecord::Schema.define(version: 2020_07_09_054246) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -27,10 +49,10 @@ ActiveRecord::Schema.define(version: 2020_06_24_071849) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "introduction"
-    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
