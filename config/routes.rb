f9c7551bce7a8ced29bdd8dd275678dd39f1d987
yuma0222 <nakamura_poison@icloud.com>
2020-07-10 01:46:09 +0900

    [Finish]re_task4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
-  get 'home/about' => 'home#about'
-  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :users, only: [:index, :show, :edit, :update] do
+  	resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
-    resources :book_comments, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
+
+  root to: 'home#index'
+  get 'home/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
